a94c7cbe63975ef371b2282a5b1a3f01f5b17c3f
Vianney HERVÉ <vianney.herve@etudiant.univ-rennes.fr>
2023-12-17 17:19:34 +0100

    ready for fight

--- controller.cpp
+++ controller.cpp
@@ -4,16 +4,26 @@
 
 namespace controller {
 
+    static const uint8_t buttons[] = {
+        (uint8_t)button::up,
+        (uint8_t)button::down,
+        (uint8_t)button::right,
+        (uint8_t)button::left,
+        (uint8_t)button::b,
+        (uint8_t)button::a,
+    };
+
     void initialize() {
-        for(button btn : global::buttons) {
-            gpio_init((uint8_t)btn);
-            gpio_set_dir((uint8_t)btn, GPIO_IN);
-            gpio_pull_up((uint8_t)btn);
+        for(uint8_t btn : buttons) {
+            gpio_init(btn);
+            gpio_set_dir(btn, GPIO_IN);
+            gpio_pull_up(btn);
         }
     }
 
     bool is_pressed(const button btn) {
-        return gpio_get((uint8_t)btn);
+        // buttons are input pull_up so it is down when we press it
+        return !gpio_get((uint8_t)btn);
     }
 
 } // namespace controller
--- main.cpp
+++ main.cpp
@@ -1,28 +1,21 @@
 #include <pico/stdlib.h>
 
 #include "driver.hpp"
-#include "sound.hpp"
 #include "controller.hpp"
 #include "colors.hpp"
+
+#include "pico_tone.hpp"
 
 int main(void) {
 	ili9341::initialize();
 	controller::initialize();
 
+	Tone player(/*pin: */ 28, 100, 100, 100, 100, 100, 100, 100);
+	player.init(/* blocking: */ false);
+
+	ili9341::clear(color::black);
+
 	while(true) {
-		if(controller::is_pressed(controller::button::up)) {
-			ili9341::clear(color::red);
-		} else if(controller::is_pressed(controller::button::down)) {
-			ili9341::clear(color::magenta);
-		} else if(controller::is_pressed(controller::button::right)) {
-			ili9341::clear(color::yellow);
-		} else if(controller::is_pressed(controller::button::left)) {
-			ili9341::clear(color::green);
-		} else if(controller::is_pressed(controller::button::a)) {
-			ili9341::clear(color::black);
-		} else if(controller::is_pressed(controller::button::b)) {
-			ili9341::clear(color::white);
-		}
-		sleep_ms(100);
+
 	}
 }
